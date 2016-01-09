d0afc25de3d9e5cc5c1db8a43a7a49ce708e12bf
Scott Bettigole <scott.bettigole@tufts.edu>
2016-01-09 01:02:07 -0500

    created the board

---
+++ BrickBreaker/BrickBreaker.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+	//var rect = {};
+	//rect = $('#rect');
+	//rects = [];
+	//rects.push(rect);
+	window_height = $(window).height();
+	for (i = 1; i < 12; i++) {
+		for (j = 0; j < 4; j++) {
+			space_down = (window_height*.1) + (j*50);
+			space_left = i*8;
+			$('<div/>', {
+				'id':'rect' + i + j,
+				'class':'rects'
+				//prop: {left: '50%'}
+			}).appendTo('body');
+			$('#rect'+i+j).css("left", space_left+'%');
+			$('#rect'+i+j).css("top", space_down+'px');
+		}
+	}
+	/*console.log(rect.css("left"));
+	rect2 = $('#rect2');
+	rect2.css("left", "60%");
+	console.log(rect.css("left"));
+	rects.push(rect2); 
+	$('<div/>', {
+	    'id':'rect2',
+	    'class':'rects',
+	})*/
+})
