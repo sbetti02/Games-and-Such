3a2d3efc039c866a551d4882f834f86c1815930e
Scott Bettigole <scott.bettigole@tufts.edu>
2016-01-16 04:05:22 -0500

    added movement for ball, rebounding off paddle and walls

--- BrickBreaker/BrickBreaker.js
+++ BrickBreaker/BrickBreaker.js
@@ -16,7 +16,6 @@ $(document).ready(function() {
 			$('#rect'+i+j).css("top", space_down+'px');
 		}
 	}
-
 	$('html').keydown(function (e) { // stay pressed
 		if (e.which == 37) {
 			if (0 < $('#paddle').position().left) {
@@ -33,8 +32,59 @@ $(document).ready(function() {
 			}
 		}
 	})
+	var count = 100;
+	function animate(movedown, moveleft, x_vel) {
+		var y_speed;
+		var x_movement;
+		var x_speed = x_vel;
+		if (moveleft) {
+			x_movement = "-="+x_vel;
+		}
+		else {
+			x_movement = "+="+x_vel;
+		}
+		if (movedown) {
+			y_speed = "+=5";
+		} 
+		else {
+			y_speed = "-=5";
+		}
+		$('#ball').animate({
+			'top': y_speed, 'left': x_movement}, 5, "swing", function() {
+				if (($('#ball').position().top > ($('#paddle').position().top - 1.5*$('#paddle').height()) && 
+					$('#ball').position().left > $('#paddle').position().left && 
+					$('#ball').position().left < ($('#paddle').position().left + $('#paddle').width()))) {
+					movedown = false;
+					var impact_location = $('#ball').position().left-$('#paddle').position().left;
+					var pct_hit = impact_location/$('#paddle').width();
+					if (pct_hit>.5) {
+						x_speed = (pct_hit-.5)*10;
+						moveleft = false;;
+					}
+					else{
+						x_speed = (.5-pct_hit)*10;
+						moveleft = true;
+					}
+				}
+				if ($('#ball').position().top < 0) {
+					movedown = true;
+				}
+				if ($('#ball').position().left < 0) {
+					moveleft = false;
+				}
+				if ($('#ball').position().left > $(window).width() - $('#ball').width()) {
+					moveleft = true;
+				}
+				if ($('#ball').position().top > ($(window).height()-$('#ball').height())) {
+					return;
+				}
+				animate(movedown, moveleft, x_speed);
+			}
+		);
+	}
+	animate(true,true,0);
 
-	
+
 	/*console.log(rect.css("left"));
 	rect2 = $('#rect2');
 	rect2.css("left", "60%");
